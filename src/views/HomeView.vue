3bc4f08b68161086b55479db52ef63cbd2d4a025
Tho Nguyen Viet <69102801+thonv0302@users.noreply.github.com>
2023-12-26 23:13:23 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { collection, query, getDocs } from 'firebase/firestore'
+import { useFirestore } from 'vuefire'
+
+const db = useFirestore()
+const q = query(collection(db, 'cafes'))
+
+const querySnapshot = await getDocs(q)
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, ' => ', doc.data())
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
